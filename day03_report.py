9d86d3dc4fd8dccb2dbef271d4efa92d555cb4b6
liminha <inhag1131@gmail.com>
2023-01-16 17:51:14 +0900

    day03 5.6 5.7 5.8

--- day03_report.py
+++ day03_report.py
@@ -1,30 +1,5 @@
-# 5.2
-# q = ['q1', 'q2', 'q3']
-# a = ['a1', 'a2', 'a3']
-#
-# for i in range(0,3):
-#  print(f'{q[i]}? \n {a[i]}')
-
-# 5.4
-letter = """
-    Dear {salutation} {name},
-    
-    Thank you for your letter. We are sorry that 
-    our {product} {verbed} in your {room}.
-    Pleas note that it should never be used in a {room},
-    especially near any {animals}.
-    
-    Send us your reciept and {amount} for shipping and handling.
-    We will send you another {product} that, in our tests, is
-    {percent}% less likely to have {verbed}.
-    
-    Thank you for your support.
-    Sincerely,
-    {spokesman}
-    {job_title}
-"""
-# 5.5
-print(letter.format(salutation = 'A', name = 'jun', product = 'C',
-                    verbed = 'D', room = 'E', animals = 'F'
-                    , amount = 'G', percent = '100',
-                    spokesman = 'H', job_title = 'I'))
+# 5.6, 5.7, 5.8
+name = ['duck', 'gourd', 'spitz']
+for i in [0,1,2]:
+ name_2 = name[i].capitalize() + 'y Mc' + name[i].capitalize() + 'face'
+ print(name_2)
---
+++ plus.py
@@ -0,0 +1,14 @@
+# \n, \t, \\ \, \'
+# replace()
+# [:] [start : ][ : end] [start : end : step]
+# len(string), string.split(arguments)
+# string.join(list), string.replace(A, B) A -> B
+# r or l, strip(' ', '\t', '\n') 양쪽 없앰. '인수' -> 제거
+# string.startswith('all') -> all로 시작하는가 T or F
+# string.endswith
+# string.find(), string.index() -> 몇 번째에 위치하는지 찾기
+# r, l 입력으로 처음부터 or 끝부터 찾기 가능
+# st.capitalize(), .title(), .upper(), .lower(), swapcase()
+# .center(길이), .ljust(), rjust()
+# format_string % data
+# %s %d %x %o %f %e %g %%
