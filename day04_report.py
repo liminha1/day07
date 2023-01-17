171258d256337a646d406f86b2bd334e6e115a9a
liminha <inhag1131@gmail.com>
2023-01-17 16:31:30 +0900

    day04 7.8 7.9

--- day04_report.py
+++ day04_report.py
@@ -1,9 +1,18 @@
-# 7.1
-year_list = list(range(2000, 2006))
-print(year_list)
-# 7.2
-year_third = year_list[3]
-print(year_third)
-# 7.3
-year_old = year_list[-1]
-print(year_old)
+# 7.8
+surprise = ["Groucho", "Chico", "Harpo"]
+print(surprise)
+# 7.9
+surprise[-1] = surprise[-1].lower()
+print(surprise)
+surprise_last = surprise[-1]
+print(surprise_last)
+surprise_last_list = list(surprise_last)
+print(surprise_last_list)
+surprise_last_rev = []
+for i in range(4,-1,-1):
+      surprise_last_rev.append(f'{surprise_last_list[i]}')
+print(surprise_last_rev)
+surprise[-1] = ''.join(surprise_last_rev)
+print(surprise)
+surprise[-1] = surprise[-1].capitalize()
+print(surprise)
