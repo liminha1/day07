8b9f261f403a29b45b2d9ea628770c2f61bd6ba8
liminha <inhag1131@gmail.com>
2023-01-17 17:30:33 +0900

    day04 8.1 8.2 8.3 8.4 8.5

--- day04_report.py
+++ day04_report.py
@@ -1,30 +1,30 @@
-# 7.10
-odd = []
-for i in range(0,10,2):
-    odd.append(i)
-print(odd)
-# 7.11
-start1 = ["fee", "fie", "foe"]
-rhymes = [
-    ("flop", "get a mop"),
-    ("fope", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done"),
-]
-start2 = "someone better"
-# 첫 번째
+# 8.1
+e2f = {"dog" : "chien", "cat" : "chat", "walrus" : "morse"}
+print(e2f)
+# 8.2
+print(e2f["walrus"])
+# 8.3
+f2e = list(e2f.items())
+print(f2e)
+f2e_mix = []
 for i in range(0,3):
-    start1[i] = start1[i].capitalize() + '! '
-print(start1)
-rhymes_list = []
-for i in range(0,6):
-    rhymes_list.append(list(rhymes[i]))
-    rhymes_list[i][0] = rhymes_list[i][0].capitalize() + '!'
-print(rhymes_list)
-print(''.join(start1), end='')
-print(rhymes_list[0][0])
-# 두 번째
-print(start2 + ' ', end ='')
-print(rhymes_list[0][1] + '.')
+     f2e_mix.append(list(f2e[i]))
+print(f2e_mix)
+f2e_key = []
+for i in range(0,3):
+    f2e_key.append(f2e_mix[i][0])
+print(f2e_key)
+f2e_vel = []
+for i in range(0,3):
+    f2e_vel.append(f2e_mix[i][1])
+print(f2e_vel)
+f2e = {
+    f2e_vel[0] : f2e_key[0],
+    f2e_vel[1] : f2e_key[1],
+    f2e_vel[2] : f2e_key[2],
+}
+print(f2e)
+# 8.4
+print(e2f["dog"])
+# 8.5
+print(', '.join(list(e2f.keys())))
